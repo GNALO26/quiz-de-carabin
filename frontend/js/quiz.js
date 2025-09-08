c6221cf06e53bde5212a418a7ad711705193dc28
Alfred SOSSA <alfredsossa17@gmail.com>
2025-09-09 00:30:28 +0100

    Quiz de carabin

--- frontend/js/quiz.js
+++ frontend/js/quiz.js
@@ -346,7 +346,7 @@ export class Quiz {
             `;
 
             if (!isCorrect) {
-                resultsHTML += <p class="correct"><strong>Réponse correcte:</strong> ${correctAnswerText}</p>;
+                resultsHTML += `<p class="correct"><strong>Réponse correcte:</strong> ${correctAnswerText}</p>;`
             }
 
             resultsHTML += `
